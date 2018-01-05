dd7c1712e47bf0c688c5c6364678da5f7c13fad2
TesmanovicD <danites007@gmail.com>
2018-01-05 15:35:39 +0100

    Product details

---
+++ loadBag.php
@@ -0,0 +1,103 @@
+<html>
+<head>
+  <title>Laptop Torbe - Online prodaja laptop torbi i rusaka | Katalog proizvoda</title>
+  <meta charset="utf-8">
+  <meta name="description" content="Laptop Torbe katalog sa proizvodima iz naše široke ponude asortimana raznih poznatih brendova">
+  <meta name="keywords" content="torbe za laptop,hp torbe za laptop,torbe laptop,ranac,laptop,jeftine torbe online,prodaja torbi povoljno,sve za laptop,ranac za laptop,rancevi prodaja,asus torbe, acer torbe,lenovo torbe,hp torbe,brendirane torbe">
+  <meta name="author" content="Laptop Torbe">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FACEBOOK OPEN GRAPH META TAGS -->
+  <meta property="og:url" content="http://laptop-torbe.me/" />
+  <meta property="og:title" content="Laptop Torbe - Katalog proizvoda" />
+  <meta property="og:description" content="Laptop Torbe je online prodavnica koja pruza kupovinu laptop torbi i rusaka sirokog asortimana i raznih brendova po najpovoljnijim cenama" />
+  <meta property="og:image" content="img/banner_laptop_torbe.png" />
+
+  <!-- FONT SCHOOLBELL-->
+  <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Reenie+Beanie|Imprima|Schoolbell" rel="stylesheet">
+  <!-- BOOTSTRAP CSS-->
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+  <!-- FONT AWESOME FOR ICONS -->
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+  <!-- NORMALIZE.CSS GLITCH FIX -->
+  <link rel="stylesheet" type="text/css" href="css/normalize.css">
+  <!-- PERSONAL - LOCAL CSS -->
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+
+  <!-- JQUERY SCRIPT-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!-- JQUERY SLIM-->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+  <!-- POOPER JS FOR IMG SLIDER-->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+  <!-- BOOTSTRAP SCRIPT-->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <!-- LOCAL DATABASE OF LAPTOPS -->
+  <script src="database.js"></script>
+  <!-- SCRIPT FOR LOADING FROM DATABASE-->
+  <script src="js/loadBags.js"></script>
+  <!-- PERSONAL SCRIPT FILE-->
+  <script src="js/javascriptfile.js"></script>
+</head>
+
+<body>
+  <nav class="navbar navbar-inverse navbar-static-top" >
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">Laptop-Torbe.me</a>
+      </div><!--end of navbar-header-->
+
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav" id="itemscentered">
+          <li><a href="/">Početna stranica &nbsp;&nbsp;<span class=" glyphicon glyphicon-home"></span></a></li>
+          <li class="active"><a href="laptop-torbe.html">Naši proizvodi &nbsp;&nbsp;<span class=" glyphicon glyphicon-briefcase"></span></a></li>
+          <li><a href="kontakt.html">Kontakt &nbsp;&nbsp;<span class="  glyphicon glyphicon-phone-alt"></span></a></li>
+          <li><a href="onama.html">O nama &nbsp;&nbsp;<span class="  glyphicon glyphicon-education"></span></a></li>
+        <li><a href="korisnik/registracija.html">Registracija &nbsp;&nbsp;<span class="glyphicon glyphicon-user"></span></a></li>
+        <li><a href="korisnik/login.html">Prijava &nbsp;&nbsp;<span class="glyphicon glyphicon-log-in"></span></a></li>
+        </ul><!--end of navbar items-->
+      </div><!--end of myNavbar-->
+    </div><!--end of container-fluid-->
+  </nav><!--end of navbar-->
+
+<?php
+include_once("./config/dbconfig.php");
+
+if(!isset($_GET['id'])) {
+  header("Location:./index.php");
+} else {
+  $torbaID = $_GET['id'];
+  $sql_select = "SELECT * FROM `stavke_torbe` WHERE ID=".$torbaID."";
+  $result = mysqli_query($connection,$sql_select);
+
+  if (mysqli_num_rows($result) > 0) {
+      while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+          echo "Naziv ". $row['Naziv']."<br/>";
+          echo "<img src=\"./img/".$row['Slika']."\" alt=\"".$row['Alt']."\"><br/>";
+          echo "Opis: ".$row['Opis']."<br/>";
+          echo "Cena: ".$row['Cena']."<br/>";
+          echo "Dostupno: ".$row['Kolicina']."kom.<br/>";
+      }
+  } else echo "Nema torbe sa tim ID-em u bazi";
+
+}
+
+ ?>
+
+ <div id="footer">
+  <div class="text-center center-block">
+    <h4 style="font-family: Reenie Beanie;font-size:20px;"> Laptop-Torbe.rs </h4>
+  <h4 style="font-family: Amatic SC;font-size:25px;">ŠKOLSKI PROJEKAT</h4>
+    <a href="https://www.facebook.com"><i class="fa fa-facebook-square fa-3x social"></i></a>
+    <a href="https://twitter.com"><i class="fa fa-twitter-square fa-3x social"></i></a>
+    <a href="https://instagram.com"><i class="fa fa-instagram fa-3x social"></i></a>
+  </div><!--end of center-block-->
+ </div><!--end of footer-->
+</body>
+ </html>
