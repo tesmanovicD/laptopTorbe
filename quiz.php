11c5feb7b486579b5e4dea18bc20aed37b046fbb
TesmanovicD <danites007@gmail.com>
2018-01-31 12:48:51 +0100

    Zamena lokalnog definisanja komunikacije sa bazom sa includovanjem dbconfig fajla

--- quiz.php
+++ quiz.php
@@ -1,13 +1,5 @@
 <?php
-
-$DB_HOSTNAME = "localhost";
-$DB_USERNAME = "root";
-$DB_PASSWORD = "";
-$DB_DATABASE = "laptop-torbe";
-
-global $connection;
-$connection  = mysqli_connect($DB_HOSTNAME, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
-
+include_once("./config/dbconfig.php");
 
 //Creating Array for JSON response
 
